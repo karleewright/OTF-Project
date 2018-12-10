880b029f17404ba7ab886d92daca6beb1bbbc984
Karlee <karleewright87@gmail.com>
2018-12-10 15:35:58 -0700

    delete call works

--- project-ui/src/Components/StatsTable/StatsTable.js
+++ project-ui/src/Components/StatsTable/StatsTable.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-dupe-class-members */
 // https://medium.com/front-end-hacking/tested-react-lets-build-a-data-table-a76aa100d23f
 
 // import {API Object}
 
-
-
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import ReactTable from "react-table";
-import 'react-table/react-table.css'
+import "react-table/react-table.css";
 
 /* What data do you want to manipulate
             
@@ -18,277 +17,275 @@ import 'react-table/react-table.css'
             
 */
 class StatsTable extends Component {
-    constructor( props ) {
-        super();
-        this.state = {
-            data:[ ],
-            _id: "id"
-          }
-
-
-
-
-
-    }
-    
-    componentDidMount(){
-      const url = "http://localhost:4000/stats";
-      fetch(url, {
-        method: "GET"
-      }).then(response => response.json()).then(data => {
-        // console.log("data",data)
-        this.setState({data: data})
-      })
-
-    }
-    
-   
-    
-       
-    
-  
-    
-    render() {
-     
-      
-      return (
-        <div>
-          <ReactTable
-            data={this.state.data}
-            _id= {this.state.data._id}
-            columns={[
-              {
-                Header: "Date",
-                columns: [
-                  {
-                    Header: "Date",
-                    accessor: "Date",
-                    sortable: true,
-                    filterable: true
-                    
-                  }
-                ]
-              },
-
-
-              {
-                Header: "Calories Burned",
-
-                columns: [
-                  {
-                    Header: "Calories Burned",
-                    accessor: "Burned",
-                    sortable: true,
-                    filterable: true,
-                    width: 100,
-                    maxWidth: 100,
-                    minWidth: 100,
-
-                  },
-                ]
-              },
-
-
-              {
-                Header: 'Ponts Earned',
-                columns: [
-                  {
-                    Header: "Low",
-                    accessor: "Low",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "Med",
-                    accessor: "Med",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "High",
-                    accessor: "High",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "Critical",
-                    accessor: "Critical",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "Extreme",
-                    accessor: "Extreme",
-                    sortable: false,
-                    filterable: false
-                  },
-                ]
-              },
-
-              {
-                Header: 'Heart Rate',
-                columns: [
-                  {
-                    Header: "Average",
-                    accessor: "AvgHR",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "Peak HeartRate",
-                    accessor: "PeakHR",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "Avg. % Max HR",
-                    accessor: "AvgPercMaxHR",
-                    sortable: false,
-                    filterable: false
-                  },
-                  {
-                    Header: "MaxHR",
-                    accessor: "MaxHR",
-                    sortable: false,
-                    filterable: false 
-                  }
-                  ]
-                },
-
-                { 
-                    Header: "Actions",
-                    columns: [
-                    {
-                      Header: "Add",
-                      sortable: false,
-                      fitlerable: false,
-                      width: 100,
-                      maxWidth: 100,
-                      minWidth: 100,
-                      Cell: props => {
-                        return(
-                          <button style= {{backgroundColor: "green"}} >Add </button>
-                        )
-                      }
-                    },
-                      {
-                        Header: "Edit",
-                        sortable: false,
-                        fitlerable: false,
-                        width: 100,
-                        maxWidth: 100,
-                        minWidth: 100,
-                        Cell: props => {
-                          return(
-                            <button style= {{backgroundColor: "blue"}} >Edit </button>
-                          )
-                        }
-                      },
-                      {
-                        Header: "Delete",
-                        sortable: false,
-                        fitlerable: false,
-                        width: 100,
-                        maxWidth: 100,
-                        minWidth: 100,
-                        Cell: props => {
-                          return(
-                            <button style= {{backgroundColor: "red"}} >Delete </button>
-                          )}
-                      } 
-                ]
-              }
-            ]}
-            defaultPageSize={10}
-            className="-striped -highlight"
-          />
-          <br />
-      
-       
-        </div>
-      );
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      data: [],
+      isLoaded: true
+    };
   }
+
+  componentDidMount() {
+    // console.log("data is logging", data);
+    const url = "http://localhost:4000/stats";
+    fetch(url, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ data: data });
+      });
+  }
+
+  deleteStat = async (event) => {
+    console.log(event.target.value);
+    
+    const url = "http://localhost:4000/stats/id/" + event.target.value;
+    await fetch(url, {
+    method: "DELETE"
+    }).then(res => res.json())
+  };
+
+
+
+  addStat = () => {
+    console.log("add Stat Clicked");
+  };
   
+  
+  editStat = () => {
+    console.log("edit Stat Clicked");
+  };
 
 
 
-  export default StatsTable;
+  render() {
+    console.log("logs", this.state.data);
 
-
-
-
-
-
-    //What functions to manipulate the data?
-
-    //for loop
-    // Map
-
-
-    //API UPDATES -MODAL LOAD
-    //BUTTON- onClickDelete will update by table id cell/row/column?
-    //BUTTON- onClickUpdate will update by table id cell/row/column?
-    //BUTTON- onClickPost will update by table id cell/row/columnn?
-
-
-
-    
+    return (
+      <div>
+        <ReactTable
+          data={this.state.data}
     
 
+          columns={[
+            {
+              Header: "Date",
+              columns: [
+                {
+                  Header: "Date",
+                  accessor: "Date",
+                  sortable: true,
+                  filterable: true
+                }
+              ]
+            },
 
-//<ViewStats data={this.state.data}/> 
+            {
+              Header: "Calories Burned",
 
-              
+              columns: [
+                {
+                  Header: "Calories Burned",
+                  accessor: "Burned",
+                  sortable: true,
+                  filterable: true,
+                  width: 100,
+                  maxWidth: 100,
+                  minWidth: 100
+                }
+              ]
+            },
+
+            {
+              Header: "Ponts Earned",
+              columns: [
+                {
+                  Header: "Low",
+                  accessor: "Low",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "Med",
+                  accessor: "Med",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "High",
+                  accessor: "High",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "Critical",
+                  accessor: "Critical",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "Extreme",
+                  accessor: "Extreme",
+                  sortable: false,
+                  filterable: false
+                }
+              ]
+            },
+
+            {
+              Header: "Heart Rate",
+              columns: [
+                {
+                  Header: "Average",
+                  accessor: "AvgHR",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "Peak HeartRate",
+                  accessor: "PeakHR",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "Avg. % Max HR",
+                  accessor: "AvgPercMaxHR",
+                  sortable: false,
+                  filterable: false
+                },
+                {
+                  Header: "MaxHR",
+                  accessor: "MaxHR",
+                  sortable: false,
+                  filterable: false
+                }
+              ]
+            },
+
+            {
+              Header: "Actions",
+              columns: [
+                {
+                  Header: "Add",
+                  sortable: false,
+                  fitlerable: false,
+                  width: 100,
+                  maxWidth: 100,
+                  minWidth: 100,
+                  Cell: props => {
+                    return (
+                      <button
+                        onClick={this.addStat}
+                        style={{ backgroundColor: "green" }}
+                        value={props.value}
+                      >
+                        Add
+                      </button>
+                    );
+                  }
+                },
+                {
+                  Header: "Edit",
+                  sortable: false,
+                  fitlerable: false,
+                  width: 100,
+                  maxWidth: 100,
+                  minWidth: 100,
+                  Cell: props => {
+                    return (
+                      <button
+                        onClick={this.editStat}
+                        style={{ backgroundColor: "blue" }}
+                        value={props.value}
+                      >
+                        Edit{" "}
+                      </button>
+                    );
+                  }
+                },
+                {
+                  Header: "Delete",
+                  sortable: false,
+                  fitlerable: false,
+                  width: 100,
+                  maxWidth: 100,
+                  minWidth: 100,
+                  accessor: "_id",
+                  Cell: props => {
+                    return (
+                      <button
+                        onClick={this.deleteStat}
+                        index={this.state.data._id}
+                        style={{ backgroundColor: "red" }}
+                        value={props.value}
+                      >
+                        Delete
+                      </button>
+                    );
+                  }
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default StatsTable;
+
+//What functions to manipulate the data?
+
+//for loop
+// Map
+
+//API UPDATES -MODAL LOAD
+//BUTTON- onClickDelete will update by table id cell/row/column?
+//BUTTON- onClickUpdate will update by table id cell/row/column?
+//BUTTON- onClickPost will update by table id cell/row/columnn?
+
+//<ViewStats data={this.state.data}/>
+
 //* taking the "data" and setting it to a NEW object this.state.stats to be passed around and used by children within the StatsTable Component */
 
-//<ViewStats data={this.state.stats}/>   
+//<ViewStats data={this.state.stats}/>
 
+// constructor(){
+// super();
+// this.state = {
+//         Date: " ",
+//         Burned: " ",
+//         Low: " ",
+//         Med: " ",
+//         High: " ",
+//         Critical: " ",
+//         Extreme: " ",
+//         AvgHR: " ",
+//         PeakHR: " ",
+//         AvgPercMaxHR: " ",
+//         MaxHR: " ",
 
+//         loaded : false,
 
+//         data : []
+//         //modalIsOpen : false
+//         //statsLoaded : true for my GET
+//         //Add StatButton :
+//         //Delete Stat Button
 
+//     };
+//     //default loaded is set to false can it ever be set to open? This sets state for the props so when the stats load we need to update this to true for it to work?
 
+//     // this.handleClick = this.handleClick.bind(this) //when I say handleClick I want you to think of this class, and .bind(what?- this)
+// }
 
+// }
 
-
-
-
-
-    // constructor(){
-    // super();
-    // this.state = {
-    //         Date: " ",
-    //         Burned: " ",
-    //         Low: " ",
-    //         Med: " ",
-    //         High: " ",
-    //         Critical: " ",
-    //         Extreme: " ",
-    //         AvgHR: " ",
-    //         PeakHR: " ",
-    //         AvgPercMaxHR: " ",
-    //         MaxHR: " ",
-
-    //         loaded : false,
-
-
-    //         data : []
-    //         //modalIsOpen : false
-    //         //statsLoaded : true for my GET
-    //         //Add StatButton : 
-    //         //Delete Stat Button
-
-
-    //     };
-    //     //default loaded is set to false can it ever be set to open? This sets state for the props so when the stats load we need to update this to true for it to work?
-            
-    //     // this.handleClick = this.handleClick.bind(this) //when I say handleClick I want you to think of this class, and .bind(what?- this)
-    // }
-
-    
-    // }
-
-    
 // import React, { Component } from 'react';
 // import StatsRow from './StatsRow'
 // // import DeleteStats from './DeleteStats';
@@ -299,27 +296,20 @@ class StatsTable extends Component {
 // import { MDBCard, MDBCardBody, MDBCardHeader, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead  } from 'mdbreact';
 
 // class MyStats extends Component {
-//    
+//
 
-
-
-        //data_panel = {
+//data_panel = {
 //       columns : this.state.columns,
 //       rows : this.state.rows,
 
 //       id : this.state.id
 
-//       label: 
+//       label:
 //       field:
-//       sort: 
-
+//       sort:
 
 //    }
 
-
-  
-
- 
 // StatsTable = (props) => {
 //   const data_panel = {
 //     columns: [
@@ -358,13 +348,13 @@ class StatsTable extends Component {
 //         'field': 'Critical',
 //         'sort': 'asc'
 //       },
-      
+
 //       {
 //         'label': 'Minutes in Extreme',
 //         'field': 'Extreme',
 //         'sort': 'asc'
 //       },
-    
+
 //       {
 //         'label': 'AverageHR',
 //         'field': 'AvgHR',
@@ -385,7 +375,7 @@ class StatsTable extends Component {
 //         'field': 'MaxHR',
 //         'sort': 'asc'
 //       }],
-    
+
 //     //How to account for checkboxs as dataset gets bigger? Start at checkbox1 and run a loop function to add checkbox# for every entry added? Set id= db generated ID?
 
 //     rows: [
@@ -405,21 +395,17 @@ class StatsTable extends Component {
 //       }]
 //   };
 
-
 //     render()
-//     //by calling const stats we are taking the stats from our returned {stats : myJson} of the API 
+//     //by calling const stats we are taking the stats from our returned {stats : myJson} of the API
 //     //const stats gets the data from our API call, the properties of that data, maps it to the StatsRow component where it will display and assigns data now = to {mappedstat}
 //     const stats = this.props.data.map(results => <StatsRow data={results}/>)
 //         console.log (stats)
-  
-
 
 //     return(
 //     <MDBCard narrow>
 //       <MDBCardHeader className="view view-cascade gradient-card-header orange d-flex justify-content-between align-items-center py-2 mx-4 mb-3">
-        
-        
-//         <a href="#" className="white-text mx-3"  > Karlees Workout Totals   </a> 
+
+//         <a href="#" className="white-text mx-3"  > Karlees Workout Totals   </a>
 //         <div>
 
 //           <MDBBtn outline rounded size="sm" color="white" className="px-2">
@@ -430,12 +416,10 @@ class StatsTable extends Component {
 //             <i className="fa fa-remove mt-0"></i>
 //           </MDBBtn>
 
-         
-
 //         </div>
 
 //       </MDBCardHeader>
-      
+
 //       <MDBCardBody cascade>
 //         <MDBTable btn fixed>
 //           <MDBTableHead columns={data_panel.columns} />
@@ -446,8 +430,5 @@ class StatsTable extends Component {
 // );
 // }
 // }
-
-
-
 
 // export default MyStats
